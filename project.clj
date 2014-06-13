5aa3f73325a9f34035928c25e1418293654e7c09
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2014-06-13 23:47:48 +0200

    do not test java class but clojure style tests

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clojure-erlastic "0.1.1"
+(defproject clojure-erlastic "0.1.2"
   :description "Micro lib making use of erlang JInterface lib to decode and
                 encode Binary Erlang Term and simple erlang port interface with
                 core.async channel. So you can communicate with erlang coroutine
                 with clojure abstraction"
   :url "https://github.com/awetzel/clojure-erlastic"
+  :scm {:name "git" :url "https://github.com/awetzel/clojure-erlastic"}
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
--- src/clojure_erlastic/core.clj
+++ src/clojure_erlastic/core.clj
@@ -18,18 +18,17 @@
                     :decoding_error))))))))))
 
 (defn encode [obj]
-  (let [t (type obj)]
+  (if (nil? obj) (new OtpErlangAtom "nil")
     (if (seq? obj) (new OtpErlangList (into-array OtpErlangObject (map encode obj)))
-      (if (= (Class/forName "[B") (type obj)) (new OtpErlangBinary (bytes obj))
-        (if (= t clojure.lang.PersistentArrayMap) (new OtpErlangMap 
-                                                       (into-array OtpErlangObject (map encode (keys obj))) 
-                                                       (into-array OtpErlangObject (map encode (vals obj))))
-          (if (= t clojure.lang.PersistentVector) (new OtpErlangTuple (into-array OtpErlangObject (map encode obj)))
-            (if (= t java.lang.String) (new OtpErlangBinary (bytes (byte-array (map byte obj))))
-              (if (= t clojure.lang.Keyword) (new OtpErlangAtom (name obj))
-                (if (= t java.lang.Double) (new OtpErlangDouble obj)
-                  (if (= t java.lang.Long) (new OtpErlangLong obj) 
-                    (new OtpErlangAtom "encoding_error")))))))))))
+      (if (vector? obj) (new OtpErlangTuple (into-array OtpErlangObject (map encode obj)))
+        (if (string? obj) (new OtpErlangBinary (bytes (byte-array (map byte obj))))
+          (if (keyword? obj) (new OtpErlangAtom (name obj))
+            (if (integer? obj) (new OtpErlangLong (long obj))
+              (if (float? obj) (new OtpErlangDouble (double obj))
+                (if (map? obj) (new OtpErlangMap (into-array OtpErlangObject (map encode (keys obj))) 
+                                                 (into-array OtpErlangObject (map encode (vals obj))))
+                  (if (= (Class/forName "[B") (type obj)) (new OtpErlangBinary (bytes obj))
+                    (new OtpErlangAtom (str obj))))))))))))
 
 (defn port-connection []
   (let [in (chan) out (chan)]
